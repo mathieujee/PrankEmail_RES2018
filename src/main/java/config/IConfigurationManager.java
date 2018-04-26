409c1c5dac046025eeb90e204811c7848510447f
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2018-04-26 21:34:46 +0200

    IConfigMana comment

--- src/main/java/config/IConfigurationManager.java
+++ src/main/java/config/IConfigurationManager.java
@@ -4,22 +4,58 @@ import model.email.*;
 import java.io.IOException;
 import java.util.List;
 
-
+/**
+ * Manage the configuration for the prank with extern files.
+ */
 public interface IConfigurationManager {
 
+    /**
+     * Creat a List with on the file
+     *
+     * @param filename the path to the file
+     * @return a List of people
+     * @throws IOException
+     */
     public List<Person> victims(String filename) throws IOException;
 
+    /**
+     * Return a List with victims
+     * @return a List with victims
+     */
     public List<Person> getVictims();
 
+    /**
+     * Creat a List with people on the file
+     *
+     * @param filename the path to the file
+     * @return a List with people on the file
+     * @throws IOException
+     */
     public List<String> messages(String filename) throws IOException;
 
+    /**
+     * Return a List with messages
+     * @return a List with messages
+     */
     public List<String> getMessages();
 
     public List<Person> getwitnessesToCC();
 
+    /**
+     * Return the SMTP server port
+     * @return the SMTP server port
+     */
     public int getSmtpServerPort();
 
+    /**
+     * Return the number of groups
+     * @return the number of groups
+     */
     public int getNumberOfGroups();
 
+    /**
+     * Return the SMTP server address
+     * @return the SMTP server address
+     */
     public String getSmtpServerAddress();
 }
