584d281fb99d4d0337890cd719594a5ebd8732b6
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2018-04-26 21:43:13 +0200

    Person comment

--- src/main/java/model/email/Person.java
+++ src/main/java/model/email/Person.java
@@ -1,29 +1,54 @@
 package model.email;
 
+/**
+ * Represent a person
+ */
 public class Person {
 
     private String firstName;
     private String lastName;
     private String email;
 
+    /**
+     * Creat a person
+     * @param firstName
+     * @param lastName
+     * @param email
+     */
     public Person(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName  = lastName;
         this.email     = email;
     }
 
+    /**
+     * Creat a person with only email
+     * @param email
+     */
     public Person(String email) {
         this.email = email;
     }
 
+    /**
+     * Return firstname
+     * @return firstname
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Return lastname
+     * @return lastname
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Return email
+     * @return email
+     */
     public String getEmail() {
         return email;
     }
