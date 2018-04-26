1a6edb449da595758ac0c524de2eafe6e3555eb3
lionel.burgbach <lionel.burgbacher@heig-vd.ch>
2018-04-26 21:26:00 +0200

    ISmtpCLient commnent

--- src/main/java/smtp/protocol/ISmtpClient.java
+++ src/main/java/smtp/protocol/ISmtpClient.java
@@ -2,7 +2,14 @@ package smtp.protocol;
 
 import java.io.IOException;
 
+/**
+ * SMTP Client
+ */
 public interface ISmtpClient {
 
+    /**
+     * Send Email to a server
+     * @throws IOException
+     */
     public void sendEmail() throws IOException;
 }
